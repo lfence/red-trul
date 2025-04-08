4c0ffdac2077e33fbba2c08323b2ebafbfe436be
lfence <>
2025-04-08 22:01:26 +0200

    Proper error when mp3 tags are missing

--- trul.js
+++ trul.js
@@ -300,7 +300,7 @@ async function analyzeFileList(inDir, fileList) {
 
     // this was originally in parallel, but for >100 files it became flaky..
     const info = await probeMediaFile(absPath)
-    const tags = Object.keys(info.format.tags).map((key) => key.toUpperCase())
+    const tags = Object.keys(info.format.tags || {}).map((key) => key.toUpperCase())
     if (!["TITLE", "ARTIST", "ALBUM", "TRACK"].every((t) => tags.includes(t))) {
       throw new Error(`[!] Required tags are not present! check ${absPath}`)
     }
